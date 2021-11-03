5ef5f2c613db8fd7c542e0361313ffcf5f7270df
Al Fajr <62805347+aN4ksaL4y@users.noreply.github.com>
2021-11-03 13:17:35 +0700

    go to v0.2

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup
 
 setup(
-
+  version='0.2',
+  author='Muhammad Al Fajri',
+  author_email='fajrim228@gmail.com',
+  description='Python script dengan tempo yang sesingkat-singkatnya.',
+  install_requires=['bs4', 'requests', 'urwid'],
+  scripts=['bin/singgalang'],
+  zip_safe=False
 )
